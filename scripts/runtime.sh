12c5d60d4705db124d142d1b00cbe2126be55419
Laszlo Lueck <llueck@gmail.com>
2023-04-27 20:39:09 +0000

    extend user check

--- scripts/runtime.sh
+++ scripts/runtime.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-if ! grep "${USERNAME}" /etc/passwd >dev/null 2>&1; then
-	useradd -rm -d /home/${USERNAME} -s /bin/bash -g root -G sudo -u 1000 ${USERNAME}
-	echo "${USERNAME}:${PASSWORD}" | chpasswd
+if id "${USERNAME}" &>/dev/null; then
+  echo "User ${USERNAME} exists do nothing"
+else
+  echo "User ${USERNAME} does not exists do nothing"
+  useradd -rm -d /home/${USERNAME} -s /bin/bash -g root -G sudo -u 1000 ${USERNAME}
+  echo "${USERNAME}:${PASSWORD}" | chpasswd
 fi
 /usr/sbin/sshd -D
