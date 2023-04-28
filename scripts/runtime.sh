68cf5f25cfa4c9d97034b7ca1d608dfdaa26b7ec
Laszlo Lueck <llueck@gmail.com>
2023-04-28 08:53:08 +0200

    fix typo

--- scripts/runtime.sh
+++ scripts/runtime.sh
@@ -3,8 +3,9 @@
 if id "${USERNAME}" &>/dev/null; then
   echo "User ${USERNAME} exists do nothing"
 else
-  echo "User ${USERNAME} does not exists do nothing"
+  echo "User ${USERNAME} does not exists lets create them"
   useradd -rm -d /home/${USERNAME} -s /bin/bash -g root -G sudo -u 1000 ${USERNAME}
-  echo "${USERNAME}:${PASSWORD}" | chpasswd
 fi
+echo "${USERNAME}:${PASSWORD}" | chpasswd
+
 /usr/sbin/sshd -D
