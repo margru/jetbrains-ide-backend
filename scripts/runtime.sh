0c89a2e90e51302dd0d603bef0acdb009c181999
Laszlo Lueck <llueck@gmail.com>
2023-04-28 09:02:57 +0200

    fix some things

--- scripts/runtime.sh
+++ scripts/runtime.sh
@@ -4,7 +4,8 @@ if id "${USERNAME}" &>/dev/null; then
   echo "User ${USERNAME} exists do nothing"
 else
   echo "User ${USERNAME} does not exists lets create them"
-  useradd -rm -d /home/${USERNAME} -s /bin/bash -g root -G sudo -u 1000 ${USERNAME}
+  useradd -rm -d /home/"${USERNAME}" -s /bin/bash -g root -G sudo -u 1000 "${USERNAME}"
+  chown "${USERNAME}" /home/"${USERNAME}"
 fi
 echo "${USERNAME}:${PASSWORD}" | chpasswd
 
